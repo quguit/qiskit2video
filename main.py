6c88e79a9596efb795d8ee5a91b090ca121f77ed
quguit <engguilhermen@gmail.com>
2026-06-21 10:45:39 -0300

    Add partial movie file list for BlochSphereScene in 1080p60 directory

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from qiskit2video import *
 
 # v1.0: escopo de 1 qubit apenas
-qreg_q = QuantumRegister(1, 'q')
+qreg_q = QuantumRegister(1, "q")
 circuit = QuantumCircuit(qreg_q)
 
 # por padrão o qubit começa em |0>
-circuit.h(qreg_q[0])   # Hadamard
-circuit.s(qreg_q[0])   # fase S
+circuit.h(qreg_q[0])  # Hadamard
+circuit.h(qreg_q[0])  # Hadamard
+circuit.x(qreg_q[0])  # NOT
 
-circuit.video(5, 12)   # (velocidade da transição, frames por transição)
+circuit.video(5, 12)  # (velocidade da transição, frames por transição)
